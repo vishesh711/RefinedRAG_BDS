10ed53a54c0a0ed6533f6494895663cd21c4e5c0
Vishesh Kumar <mail4vishesh@gmail.com>
2024-12-12 22:34:45 -0500

    Add files via upload

---
+++ notebooks/cosine_similarity.py
@@ -0,0 +1,39 @@
+import os
+import json
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Initialize the Langchain OpenAI Embedding API
+embeddings = FastEmbedEmbeddings(model_name="BAAI/bge-base-en-v1.5")
+
+# Sample texts
+text1 = "What large language models are discussed in the class? List all the LLMs."
+text2 = """also keep developing. So the column three now we are using now is very powerful multimodal AI. Now AI
+can not only just do text but they can do image and audio. So here actually you can directly say audio to
+it. Okay, so this is uh two. How do you like it? Yeah, this one is not that good, right. But this one seems
+better, you know, but it's also the number are not that good. So this is an area they can further improve.
+Finally, let's see the answer from Gemini Gemny gave us answer, right. Alpha. Go zero. Right. Okay.
+From here you can see some preference, right. Some buyers. So each company's two is certainly biased
+towards a product of that company, Alpha. Right. Lambda. So different tools. Okay. Yes, darling. Yeah, the
+text is darling and also is video generation tools. Right. So also this is very hard I think what's the name of
+the company? So last year there was a very, let me see. So maybe I can find it for you guys. This remind
+me that. What's that it. Yeah, I just forgot the name. Anyone can remind me. Here was a. What is that?
+Yeah, somebody says going deeper. No, that's. Yeah, yeah. Nevermind. So basically the reason. Yeah,
+Sora. Yes. Yeah. Thank you. So let's see what's sora currently doing? Sora is part of OpenAI, but the
+reason I want to mention this one because even for Sora, for all those video generating also how to
+handle text is a big issue because here if you look at the traditional ones, like an image, right. So even
+though there may be some issues right in the background, right. So we cannot tell. But if there is some
+word there, some text there, we can easily tell whether it is good or bad. Right. Like this one. Right. For
+other part you feel good, but for the text you can easily see the issues. Okay. This is also because they
+didn't use a separate text generator for the text, I think that's the root cause of the issue. Okay, let's come
+back. Okay, so with all those great improvement, right, so what I want to share with you the, the slides,"""
+
+# Vectorize the texts
+vector1 = embeddings.embed_query(text1)
+vector2 = embeddings.embed_query(text2)
+
+# Compute the cosine similarity
+cosine_sim = cosine_similarity([vector1], [vector2])
+
+# Print the cosine similarity
+print("Cosine Similarity:", cosine_sim[0][0])
---
+++ notebooks/similarity.py
@@ -0,0 +1,193 @@
+import os
+import json
+import time
+from langchain_community.document_loaders import PyPDFium2Loader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+from langchain_groq import ChatGroq
+from langchain_community.chat_models import ChatOllama
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain_community.vectorstores import Qdrant
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_core.language_models import BaseLanguageModel
+from langchain_core.vectorstores import VectorStore, VectorStoreRetriever
+from langchain_community.vectorstores import Annoy
+
+import warnings
+warnings.filterwarnings('ignore')
+
+def load_document(file_path: str) -> str:
+    """
+    Loads a document from the given file path.
+    """
+    loaded_documents = PyPDFium2Loader(file_path).load()
+    document_text = "\n".join([doc.page_content for doc in loaded_documents])
+
+    return document_text
+
+def create_embeddings() -> FastEmbedEmbeddings:
+    """
+    Initializes and returns the FastEmbedEmbeddings instance.
+    """
+    return FastEmbedEmbeddings(model_name="BAAI/bge-base-en-v1.5")
+
+def create_vector_db(documents: list, audio_name: str) -> Qdrant:
+    """
+    Creates a vector store (Qdrant) and returns the vector database.
+    """
+    embeddings = create_embeddings()
+    vector_db = Qdrant.from_documents(
+        documents=documents,
+        embedding=embeddings,
+        path='data/embeddings',
+        collection_name=f"document_collection_{audio_name}"
+    )
+    return vector_db
+
+
+
+def retreive_similar_docs_hybrid_Search(prompt: str, vector_db: Qdrant) -> str:
+
+    """
+    Retreive vectors similar to the prompt from the vector db
+    """
+    retriever = vector_db.as_retriever(
+                search_type="similarity", search_kwargs={"body_search": "alpha"}
+            )
+
+    search_results = retriever.invoke(prompt)
+    # Format the retrieved documents
+    context = "\n\n\n".join([result.page_content for result in search_results])
+
+    return context
+
+def retreive_similar_docs_hnsw(prompt: str, vector_db: Qdrant) -> str:
+
+    """
+    Retreive vectors similar to the prompt from the vector db
+    """
+
+    # Perform similarity search on the vector database
+    search_results = vector_db.similarity_search([prompt], k=3)
+
+    # Format the retrieved documents
+    context = "\n\n\n".join([result.page_content for result in search_results])
+
+    return context
+
+def retreive_similar_docs_mmr(prompt: str, vector_db: Qdrant) -> str:
+
+    """
+    Retreive vectors similar to the prompt from the vector db
+    """
+    retriever = vector_db.as_retriever(
+            search_type="mmr", search_kwargs={"k": 3}
+        )
+
+    search_results = retriever.invoke(prompt)
+    # print(search_results)
+
+    # Format the retrieved documents
+    context = "\n\n\n".join([result.page_content for result in search_results])
+
+    return context
+
+def retreive_similar_docs_similarity_score_threshold(prompt: str, vector_db: Qdrant) -> str:
+
+    """
+    Retreive vectors similar to the prompt from the vector db
+    """
+
+    retriever = vector_db.as_retriever(
+            search_type="similarity_score_threshold", search_kwargs={"score_threshold": 0.6, "k": 3}
+        )
+
+    search_results = retriever.invoke(prompt)
+    # print(search_results)
+
+    # Format the retrieved documents
+    context = "\n\n\n".join([result.page_content for result in search_results])
+
+    return context
+
+
+def retreive_similar_docs_top_k(prompt: str, vector_db: Qdrant) -> str:
+
+    """
+    Retreive vectors similar to the prompt from the vector db
+    """
+
+    retriever = vector_db.as_retriever(
+        search_type="similarity", search_kwargs={"k": 3}
+    )
+
+    search_results = retriever.invoke(prompt)
+    # print(search_results)
+
+    # Format the retrieved documents
+    context = "\n\n\n".join([result.page_content for result in search_results])
+
+    return context
+
+
+def main():
+
+    # Record the start time
+    start_time = time.time()
+
+    # Load the document from the provided path
+    document_path = 'data\\transcription\\transcribed_audio_1.pdf'
+    document_text = load_document(document_path)
+
+    audio_name = document_path[-11:-4]
+    print(audio_name)
+
+    # Embed the document
+    recursive_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=2048,
+            chunk_overlap=128,
+            add_start_index=True,
+        )
+
+    recursive_chunks = recursive_splitter.split_text(document_text)
+    splits = [Document(page_content=chunk) for chunk in recursive_chunks]
+    
+    # Store documents in the vector database (Qdrant)
+    vector_db = create_vector_db(splits, audio_name)
+    print("Vector db created!\n\n")
+
+    # Get the user prompt and generate a response
+    user_prompt = """
+        What large language models are discussed in the class? List all the LLMs.
+        """
+
+    # top k
+    similarity_search_results_top_k = retreive_similar_docs_top_k(user_prompt, vector_db)    
+    print(similarity_search_results_top_k)
+
+    # # similarity score threshold
+    # similarity_search_results_similarity_Score_threshold = retreive_similar_docs_similarity_score_threshold(user_prompt, vector_db)
+    # print(similarity_search_results_similarity_Score_threshold)
+
+    # mmr
+    # similarity_search_results_mmr = retreive_similar_docs_mmr(user_prompt, vector_db)
+    # print(similarity_search_results_mmr)
+    
+    # # Approximate Nearest Neighbour
+    # similarity_search_results_hnsw = retreive_similar_docs_hnsw(user_prompt, vector_db)
+    # print(similarity_search_results_hnsw)
+    
+    # # Hybrid Search
+    # similarity_search_results_hybrid_search = retreive_similar_docs_hybrid_Search(user_prompt, vector_db)
+    # print(similarity_search_results_hybrid_search)
+
+    # Record the end time
+    end_time = time.time()
+
+    # Calculate and print the time taken
+    time_taken = end_time - start_time
+    print(f"Time taken to generate the results: {time_taken:.2f} seconds")
+
+if __name__ == "__main__":
+    main()
